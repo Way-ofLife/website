65c2da13b0c8e4b00f1ce4edf47736315d77f6a8
澁谷悠大 <univer405@gmail.com>
2020-12-16 03:26:35 +0900

    edit nuxtconfig

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -2,6 +2,7 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
+  mode: 'universal',
   target: 'static',
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
